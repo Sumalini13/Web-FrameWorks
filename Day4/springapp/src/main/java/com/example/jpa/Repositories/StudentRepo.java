8f5a0e36a4819fdd48087aca6d3a37c819fdf630
SUMALINI GOPINATHAN <113964537+Sumalini13@users.noreply.github.com>
2023-02-28 15:42:56 +0530

    Delete Day4/springapp directory

--- Day4/springapp/src/main/java/com/example/jpa/Repositories/StudentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.jpa.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.jpa.Models.Student;
-
-public interface StudentRepo extends JpaRepository<Student, Integer> {
-
-}
